51adf4e48b48a473f160ff1cacab22400423c693
Dustin Brown <dubrowgn@gmail.com>
2017-05-13 12:03:56 -0700

    Add --help and --version options

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+extern crate getopts;
+
+use getopts::Options;
 use std::env;
 use std::io;
 use std::io::Write;
@@ -13,9 +16,41 @@ mod running;
 
 use ast::*;
 
-fn parse_args() {
-	for arg in env::args().skip(1) {
-		match parsing::parse(&arg) {
+fn build_options() -> Options {
+	let mut opts = Options::new();
+
+	opts.optflag("", "help", "print this help menu");
+	opts.optflag("", "version", "print version information");
+
+	opts
+}
+
+fn print_usage() {
+	print!("Usage: {} [OPTION] EXPRESSIONS", env!("CARGO_PKG_NAME"));
+}
+
+fn print_opts(opts: &Options) {
+	print!("{}", opts.usage(""));
+}
+
+fn print_help(opts: &Options) {
+	print_usage();
+	print_opts(opts);
+}
+
+fn print_try_help() {
+	print_usage();
+	println!();
+	println!("Try '{} --help' for more information.", env!("CARGO_PKG_NAME"));
+}
+
+fn print_version() {
+	println!("{} {}", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"));
+}
+
+fn run_exprs<'a, I>(inputs: I) where I: Iterator<Item=&'a String> {
+	for str in inputs {
+		match parsing::parse(&str) {
 			Some(Ast::Expression(expr)) => {
 				match expr.run() {
 					Ok(v) => println!("{}", v),
@@ -24,8 +59,8 @@ fn parse_args() {
 			},
 			_ => {}
 		} // match
-	} // for (arg)
-} // parse_args
+	} // for
+} // run_exprs
 
 fn repl() {
 	loop {
@@ -53,9 +88,31 @@ fn repl() {
 } // repl
 
 fn main() {
-	if env::args().len() > 1 {
-		return parse_args();
+	let args: Vec<String> = env::args().collect();
+	let opts = build_options();
+
+	let matches = match opts.parse(&args[1..]) {
+		Ok(m) => { m }
+		Err(f) => {
+			println!("{}", f);
+			print_try_help();
+			return;
+		}
+	};
+
+	if matches.opt_present("help") {
+		print_help(&opts);
+		return;
 	}
 
-	repl();
+	if matches.opt_present("version") {
+		print_version();
+		return;
+	}
+
+	if matches.free.is_empty() {
+		repl();
+	} else {
+		run_exprs(matches.free.iter());
+	}
 } // main
