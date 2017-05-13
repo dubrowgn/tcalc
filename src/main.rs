2f8d78dc78e2bd0eee456541d4e57e3f7bd23c6b
Dustin Brown <dubrowgn@gmail.com>
2017-05-13 10:04:23 -0700

    Add support for passing equations via args
    $ tcalc 1+1
    2

--- src/main.rs
+++ src/main.rs
@@ -13,11 +13,21 @@ mod running;
 
 use ast::*;
 
-fn main() {
-	for arg in env::args() {
-		println!("{}", arg);
-	}
+fn parse_args() {
+	for arg in env::args().skip(1) {
+		match parsing::parse(&arg) {
+			Some(Ast::Expression(expr)) => {
+				match expr.run() {
+					Ok(v) => println!("{}", v),
+					Err(msg) => println!("{}", msg),
+				}
+			},
+			_ => {}
+		} // match
+	} // for (arg)
+} // parse_args
 
+fn repl() {
 	loop {
 		let mut line = String::new();
 
@@ -40,4 +50,12 @@ fn main() {
 			Err(msg) => println!("error: {}", msg),
 		}
 	} // loop
+} // repl
+
+fn main() {
+	if env::args().len() > 1 {
+		return parse_args();
+	}
+
+	repl();
 } // main
