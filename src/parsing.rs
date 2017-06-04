b32cb58f1f43a4c6f85e1ad35a55fd36e0668020
Dustin Brown <dubrowgn@gmail.com>
2017-06-04 12:27:43 -0700

    Refactor the parsing module

--- src/parsing.rs
+++ src/parsing.rs
@@ -17,7 +17,7 @@ impl<'a> Parser<'a> {
 	}
 
 	fn get_token(&mut self) -> Option<Token> {
-		return self.scanner.pop()
+		self.scanner.pop()
 	}
 
 	fn put_token(&mut self, t: Token) {
@@ -25,7 +25,7 @@ impl<'a> Parser<'a> {
 	}
 
 	fn expect_token(&mut self) -> Option<Token> {
-		return match self.get_token() {
+		match self.get_token() {
 			Some(t) => Some(t),
 			None => {
 				println!("Unexpected end of input");
@@ -45,7 +45,7 @@ impl<'a> Parser<'a> {
 			return Some(Ast::Expression(expr));
 		}
 
-		return None;
+		None
 	}
 
 	fn parse_command(&mut self) -> Option<Command> {
@@ -64,234 +64,142 @@ impl<'a> Parser<'a> {
 
 		self.put_token(t);
 
-		return None
+		None
 	} // parse_command
 
 	fn parse_expression(&mut self) -> Option<Expression> {
 		trace!("parse_expression");
 
-		return self.parse_bitor();
+		self.parse_bitor()
+	}
+
+	fn parse_binary<FOp, FNext>( &mut self, map_op: FOp, parse_next: FNext) -> Option<Expression>
+		where FOp: Fn(&TokenType) -> Option<BinaryOp>, FNext: Fn(&mut Parser) -> Option<Expression>
+	{
+		let mut expr = unwrap!(parse_next(self), {
+			return None;
+		});
+
+		while let Some(t) = self.get_token() {
+			let op = unwrap!(map_op(&t.token_type), {
+				self.put_token(t);
+				break;
+			});
+
+			let right = unwrap!(parse_next(self), {
+				break;
+			});
+
+			expr = Expression::Binary(Binary {
+				left: Box::new(expr),
+				op: op,
+				right: Box::new(right)
+			});
+		} // while
+
+		Some(expr)
 	}
 
 	fn parse_bitor(&mut self) -> Option<Expression> {
 		trace!("parse_bitor");
 
-		let mut expr = unwrap!(self.parse_bitxor(), {
-			return None;
-		});
-
-		while let Some(t) = self.get_token() {
-			let op;
-			match t.token_type {
-				TokenType::Pipe => op = BinaryOp::BitOr,
-				_ => {
-					self.put_token(t);
-					break;
+		self.parse_binary(
+			|tt| {
+				match tt {
+					&TokenType::Pipe => Some(BinaryOp::BitOr),
+					_ => None
 				}
-			}
-
-			let right = unwrap!(self.parse_bitxor(), {
-				break;
-			});
-
-			expr = Expression::Binary(Binary {
-				left: Box::new(expr),
-				op: op,
-				right: Box::new(right)
-			});
-		} // while
-
-		return Some(expr);
+			},
+			|p| { p.parse_bitxor() }
+		)
 	} // parse_bitor
 
 	fn parse_bitxor(&mut self) -> Option<Expression> {
 		trace!("parse_bitxor");
 
-		let mut expr = unwrap!(self.parse_bitand(), {
-			return None;
-		});
-
-		while let Some(t) = self.get_token() {
-			let op;
-			match t.token_type {
-				TokenType::Caret => op = BinaryOp::BitXor,
-				_ => {
-					self.put_token(t);
-					break;
+		self.parse_binary(
+			|tt| {
+				match tt {
+				&TokenType::Caret => Some(BinaryOp::BitXor),
+					_ => None
 				}
-			}
-
-			let right = unwrap!(self.parse_bitand(), {
-				break;
-			});
-
-			expr = Expression::Binary(Binary {
-				left: Box::new(expr),
-				op: op,
-				right: Box::new(right)
-			});
-		} // while
-
-		return Some(expr);
+			},
+			|p| { p.parse_bitand() }
+		)
 	} // parse_bitxor
 
 	fn parse_bitand(&mut self) -> Option<Expression> {
 		trace!("parse_bitand");
 
-		let mut expr = unwrap!(self.parse_shift(), {
-			return None;
-		});
-
-		while let Some(t) = self.get_token() {
-			let op;
-			match t.token_type {
-				TokenType::Ampersand => op = BinaryOp::BitAnd,
-				_ => {
-					self.put_token(t);
-					break;
+		self.parse_binary(
+			|tt| {
+				match tt {
+				&TokenType::Ampersand => Some(BinaryOp::BitAnd),
+					_ => None
 				}
-			}
-
-			let right = unwrap!(self.parse_shift(), {
-				break;
-			});
-
-			expr = Expression::Binary(Binary {
-				left: Box::new(expr),
-				op: op,
-				right: Box::new(right)
-			});
-		} // while
-
-		return Some(expr);
+			},
+			|p| { p.parse_shift() }
+		)
 	} // parse_bitand
 
 	fn parse_shift(&mut self) -> Option<Expression> {
 		trace!("parse_shift");
 
-		let mut expr = unwrap!(self.parse_add_subtract(), {
-			return None;
-		});
-
-		while let Some(t) = self.get_token() {
-			let op;
-			match t.token_type {
-				TokenType::LeftAngleBracketX2 => op = BinaryOp::LeftShift,
-				TokenType::RightAngleBracketX2 => op = BinaryOp::RightShift,
-				_ => {
-					self.put_token(t);
-					break;
+		self.parse_binary(
+			|tt| {
+				match tt {
+				&TokenType::LeftAngleBracketX2 => Some(BinaryOp::LeftShift),
+				&TokenType::RightAngleBracketX2 => Some(BinaryOp::RightShift),
+					_ => None
 				}
-			}
-
-			let right = unwrap!(self.parse_add_subtract(), {
-				break;
-			});
-
-			expr = Expression::Binary(Binary {
-				left: Box::new(expr),
-				op: op,
-				right: Box::new(right)
-			});
-		} // while
-
-		return Some(expr);
+			},
+			|p| { p.parse_add_subtract() }
+		)
 	} // parse_shift
 
 	fn parse_add_subtract(&mut self) -> Option<Expression> {
 		trace!("parse_add_subtract");
 
-		let mut expr = unwrap!(self.parse_multiply_divide(), {
-			return None;
-		});
-
-		while let Some(t) = self.get_token() {
-			let op;
-			match t.token_type {
-				TokenType::Plus => op = BinaryOp::Plus,
-				TokenType::Minus => op = BinaryOp::Minus,
-				_ => {
-					self.put_token(t);
-					break;
+		self.parse_binary(
+			|tt| {
+				match tt {
+				&TokenType::Plus => Some(BinaryOp::Plus),
+				&TokenType::Minus => Some(BinaryOp::Minus),
+					_ => None
 				}
-			}
-
-			let right = unwrap!(self.parse_multiply_divide(), {
-				break;
-			});
-
-			expr = Expression::Binary(Binary {
-				left: Box::new(expr),
-				op: op,
-				right: Box::new(right)
-			});
-		} // while
-
-		return Some(expr);
-	}
+			},
+			|p| { p.parse_multiply_divide() }
+		)
+	} // parse_add_subtract
 
 	fn parse_multiply_divide(&mut self) -> Option<Expression> {
 		trace!("parse_multiply_divide");
 
-		let mut expr = unwrap!(self.parse_exponent(), {
-			return None;
-		});
-
-		while let Some(t) = self.get_token() {
-			let op;
-			match t.token_type {
-				TokenType::Star => op = BinaryOp::Multiply,
-				TokenType::ForwardSlash => op = BinaryOp::Divide,
-				TokenType::Percent => op = BinaryOp::Modulo,
-				_ => {
-					self.put_token(t);
-					break;
+		self.parse_binary(
+			|tt| {
+				match tt {
+				&TokenType::Star => Some(BinaryOp::Multiply),
+				&TokenType::ForwardSlash => Some(BinaryOp::Divide),
+				&TokenType::Percent => Some(BinaryOp::Modulo),
+					_ => None
 				}
-			}
-
-			let right = unwrap!(self.parse_exponent(), {
-				break;
-			});
-
-			expr = Expression::Binary(Binary {
-				left: Box::new(expr),
-				op: op,
-				right: Box::new(right)
-			});
-		} // while
-
-		return Some(expr);
+			},
+			|p| { p.parse_exponent() }
+		)
 	} // parse_multiply_divide
 
 	fn parse_exponent(&mut self) -> Option<Expression> {
 		trace!("parse_exponent");
 
-		let mut expr = unwrap!(self.parse_unary(), {
-			return None;
-		});
-
-		while let Some(t) = self.get_token() {
-			let op;
-			match t.token_type {
-				TokenType::StarX2 => op = BinaryOp::Exponent,
-				_ => {
-					self.put_token(t);
-					break;
+		self.parse_binary(
+			|tt| {
+				match tt {
+				&TokenType::StarX2 => Some(BinaryOp::Exponent),
+					_ => None
 				}
-			}
-
-			let right = unwrap!(self.parse_unary(), {
-				break;
-			});
-
-			expr = Expression::Binary(Binary {
-				left: Box::new(expr),
-				op: op,
-				right: Box::new(right)
-			});
-		} // while
-
-		return Some(expr);
+			},
+			|p| { p.parse_unary() }
+		)
 	} // parse_exponent
 
 	fn parse_unary(&mut self) -> Option<Expression> {
@@ -311,13 +219,13 @@ impl<'a> Parser<'a> {
 			}
 		}
 
-		return match self.parse_unary() {
+		match self.parse_unary() {
 			Some(right) => Some(Expression::Unary(Unary {
 				op: op,
 				right: Box::new(right)
 			})),
 			None => None,
-		};
+		}
 	} // parse_unary
 
 	fn parse_primary(&mut self) -> Option<Expression> {
@@ -327,7 +235,7 @@ impl<'a> Parser<'a> {
 			return None;
 		});
 
-		return match t.token_type {
+		match t.token_type {
 			TokenType::Number { str } => {
 				match str.parse::<f64>() {
 					Ok(f) => Some(Expression::Literal(Literal::Number(f))),
